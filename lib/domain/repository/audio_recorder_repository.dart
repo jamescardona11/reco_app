99b1a03a5f93b84bdee64f38572c535910e263e6
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-19 13:42:23 -0500

    Create audio recorder repository

---
+++ lib/domain/repository/audio_recorder_repository.dart
@@ -0,0 +1,16 @@
+import 'package:audio_recorder_app/domain/models/audio_record.dart';
+import 'package:audio_recorder_app/domain/models/audio_recorder_state.dart';
+
+abstract class AudioRecorderRepository {
+  Future<void> startRecording();
+
+  Future<void> stopRecording();
+
+  Future<void> deleteRecording(String id);
+
+  Future<bool> hasPermission();
+
+  Stream<AudioRecorderState> recordingState();
+
+  Stream<List<AudioRecord>> watchRecordings();
+}
