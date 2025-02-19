67439d044b8e1f8db5d66451023b443aa39188f1
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-19 13:42:35 -0500

    Update DI

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -1,7 +1,9 @@
+import 'package:audio_recorder_app/data/audio_recorder_repository_impl.dart';
 import 'package:audio_recorder_app/data/core/database/app_database.dart';
 import 'package:audio_recorder_app/data/core/database/mock_app_database_impl.dart';
 import 'package:audio_recorder_app/data/core/http/app_http_client.dart';
 import 'package:audio_recorder_app/data/core/http/client/dio_http_client.dart';
+import 'package:audio_recorder_app/domain/repository/audio_recorder_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -15,4 +17,12 @@ AppHttpClient appHttpClient(ref) {
 @Riverpod(keepAlive: true)
 AppDatabase appDatabase(ref) {
   return MockAppDatabaseImpl();
+}
+
+@riverpod
+AudioRecorderRepository audioRecorderRepository(ref) {
+  final appDatabase = ref.watch(appDatabaseProvider);
+  final audioRecorderRepository = AudioRecorderRepositoryImpl(appDatabase);
+  // audioRecorderRepository.init();
+  return audioRecorderRepository;
 }
