31f36fc939d13d1dcc80111eed60fcc785b0a222
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-19 12:42:10 -0500

    Create DI For DB and HTTP

---
+++ lib/config/di.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'di.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appHttpClientHash() => r'e33edc21a2a0933387bd0700c56b64f2f0526930';
+
+/// See also [appHttpClient].
+@ProviderFor(appHttpClient)
+final appHttpClientProvider = Provider<AppHttpClient>.internal(
+  appHttpClient,
+  name: r'appHttpClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appHttpClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppHttpClientRef = ProviderRef<AppHttpClient>;
+String _$appDatabaseHash() => r'0c40af74d511e2169de8fc0818ce54dd13ebdbd9';
+
+/// See also [appDatabase].
+@ProviderFor(appDatabase)
+final appDatabaseProvider = Provider<AppDatabase>.internal(
+  appDatabase,
+  name: r'appDatabaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appDatabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppDatabaseRef = ProviderRef<AppDatabase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
