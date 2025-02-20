dfbd6544ef14c9a3d9cea1f355ed34dba9d83542
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-20 11:05:26 -0500

    Add new param to audio record

--- lib/domain/models/audio_record.dart
+++ lib/domain/models/audio_record.dart
@@ -1,18 +1,37 @@
 class AudioRecord {
   final String id;
   final String filePath;
+  final String? fileUrl;
   final DateTime createdAt;
 
   AudioRecord({
     required this.id,
     required this.filePath,
+    this.fileUrl,
     required this.createdAt,
   });
+
+  String get name => filePath.split('/').last;
+
+  AudioRecord copyWith({
+    String? id,
+    String? filePath,
+    String? fileUrl,
+    DateTime? createdAt,
+  }) {
+    return AudioRecord(
+      id: id ?? this.id,
+      filePath: filePath ?? this.filePath,
+      fileUrl: fileUrl ?? this.fileUrl,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
 
   factory AudioRecord.fromJson(Map<String, dynamic> json) {
     return AudioRecord(
       id: json['id'],
       filePath: json['filePath'],
+      fileUrl: json['fileUrl'],
       createdAt: json['createdAt'],
     );
   }
@@ -21,6 +40,7 @@ class AudioRecord {
     return {
       'id': id,
       'filePath': filePath,
+      'fileUrl': fileUrl,
       'createdAt': createdAt,
     };
   }
